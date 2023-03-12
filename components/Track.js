90dbcea2938fb7b9432afb13e08d004b21951f44
Gex-dev69 <dgexion@outlook.com>
2023-03-12 19:32:58 +0300

    fixed Track scroll

--- components/Track.js
+++ components/Track.js
@@ -1,44 +1,68 @@
 import React from 'react';
-import { Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, Image, StyleSheet, TouchableOpacity, View } from 'react-native';
 
-const Track = ({ name, artist, cover ,id,path,duration, onPress }) => {
+const Track = ({ name, artist, cover, id, path, duration, onPress }) => {
 
   const handlePress = () => {
-    onPress(name, artist, cover ,id,path,duration,);
+    onPress(name, artist, cover, id, path, duration,);
   }
 
   return (
-    <TouchableOpacity style={styles.container} onPress ={handlePress}>
-      {cover && <Image style={styles.cover} source={{ uri: `data:image/jpeg;base64,${cover}` }} />}
-      <Text style={styles.title}>{name}</Text>
-      <Text style={styles.artist}>{artist}</Text>
+    <TouchableOpacity style={styles.container} onPress={handlePress}>
+      <View style={styles.iconContainer}>
+        {cover === undefined || cover === null ? (
+          // Set image if the current track doesn't have art work...this doesn't work for some reason; 
+          <Image source={require('../assets/temp.png')} style={styles.cover} />
+        ) : (
+          <Image
+            source={{ uri: `data:image/jpeg;base64,${cover}` }}
+            style={styles.cover}
+          />
+        )}
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.artist}>{artist}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
-    marginVertical: 20,
-    backgroundColor:"#94430F"
+    marginVertical: 4,
+    paddingHorizontal: 10,
+    backgroundColor: "#841584",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius:10,
+    borderBottomLeftRadius:10,
+  },
+  iconContainer: {
+    marginRight: 10,
+  },
+  textContainer: {
+    flex: 1,
   },
   title: {
     color: '#FFFFFF',
     fontSize: 18,
     fontWeight: 'bold',
-    marginTop: 20,
   },
   artist: {
     color: '#BBBBBB',
     fontSize: 14,
-    marginBottom: 10,
+    marginTop: 5,
   },
   cover: {
-    width: 200,
-    height: 200,
+    width: 50,
+    height: 50,
     resizeMode: 'contain',
   },
 });
+
 
 export default Track;
